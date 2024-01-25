b3d15081385b2b68b7ae809dc82563ac92d9776c
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-25 23:49:29 +0530

    Add user model with password hashing and validation

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  phone: {
+    type: String,
+    trim: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  profile: {
+    type: String,
+    trim: true,
+  },
+  lat: {
+    type: Number,
+  },
+  lang: {
+    type: Number,
+  },
+});
+
+UserSchema.methods.createHash = async function (password) {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(password, salt);
+};
+UserSchema.methods.validatePassword = async function (incomingPassword) {
+  return await bcrypt.compare(incomingPassword, this.password);
+};
+export const User = mongoose.model('User', UserSchema);
