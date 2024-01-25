7279a17cf80153661856537a42b31cc56c4eaefe
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-26 01:35:31 +0530

    Add getNearbyUsers route and enable update route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { signin, signup } from '../controllers/userControllers.js';
+import {
+  getNearbyUsers,
+  signin,
+  signup,
+  updateUser,
+} from '../controllers/userControllers.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -7,6 +13,8 @@ router.post('/signup', signup);
 
 router.post('/signin', signin);
 
-// router.put('/update', authMiddleware, updateUser);
+router.put('/update', authMiddleware, updateUser);
+
+router.get('/nearby-users', authMiddleware, getNearbyUsers);
 
 export default router;
