c3bce89173f5d892f257be3cf8677753775657c4
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-26 01:35:54 +0530

    Update userValidation.js with validation improvements

--- backend/src/validations/userValidation.js
+++ backend/src/validations/userValidation.js
@@ -1,18 +1,25 @@
 import { z } from 'zod';
 
 export const signupSchema = z.object({
-  name: z.string().max(50).optional(),
-  email: z.string().email().min(3).max(30),
-  password: z.string().min(6),
+  name: z
+    .string()
+    .max(30, 'Name cannot be longer than 30 characters')
+    .optional(),
+  email: z
+    .string()
+    .email('Invalid email format')
+    .min(3, 'Email must be at least 3 characters')
+    .max(30, 'Email cannot be longer than 30 characters'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
   phone: z.string().optional(),
-  mobile: z.string().optional(),
-  zipCode: z.string(),
+  mobile: z.string().min(10, 'Invalid Phone number'),
+  zipCode: z.string().regex(/^\d{6}$/, 'Invalid Zip Code'),
   profile: z.string().optional(),
   lat: z.number().optional(),
   lang: z.number().optional(),
 });
 
 export const signinSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email('Invalid email format'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
 });
