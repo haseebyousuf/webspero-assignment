2383506d1d4b2318c91cb0c8a1ed8f57db150b37
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-26 17:11:30 +0530

    Update backend index.js to include file upload functionality

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from './src/routes/userRoutes.js';
+
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
-// Routes
+//set directory of where we store files
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 app.use('/user', userRoutes);
 
