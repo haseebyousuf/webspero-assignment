f7e8e1f3b0b5c90584af1a32324cb79d8efc8720
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-26 17:12:20 +0530

    Add updateSchema validation for user updates

--- backend/src/validations/userValidation.js
+++ backend/src/validations/userValidation.js
@@ -23,3 +23,22 @@ export const signinSchema = z.object({
   email: z.string().email('Invalid email format'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
 });
+
+export const updateSchema = z.object({
+  name: z
+    .string()
+    .max(30, 'Name cannot be longer than 30 characters')
+    .optional(),
+  email: z
+    .string()
+    .email('Invalid email format')
+    .min(3, 'Email must be at least 3 characters')
+    .max(30, 'Email cannot be longer than 30 characters'),
+  password: z.string().optional(),
+  phone: z.string().optional(),
+  mobile: z.string().min(10, 'Invalid Phone number'),
+  zipCode: z.string().regex(/^\d{6}$/, 'Invalid Zip Code'),
+  profile: z.string().optional(),
+  lat: z.number().optional(),
+  lang: z.number().optional(),
+});
