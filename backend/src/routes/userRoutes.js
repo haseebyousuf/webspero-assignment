2054ae0ab3804d1d79d2f4d2e8aa64a74c3c0989
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-26 17:12:16 +0530

    Add file upload functionality to user signup and update routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,14 +6,15 @@ import {
   updateUser,
 } from '../controllers/userControllers.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { upload } from '../../config/fileStorageConfig.js';
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', upload.single('profile'), signup);
 
 router.post('/signin', signin);
 
-router.put('/update', authMiddleware, updateUser);
+router.put('/update', upload.single('profile'), authMiddleware, updateUser);
 
 router.get('/nearby-users', authMiddleware, getNearbyUsers);
 
