1ec6f57067182283b0da9722d890fd66eb796298
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-26 17:12:03 +0530

    Add user signup, signin, update, and nearby users functionality

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,11 +1,20 @@
 import { User } from '../models/userModel.js';
 
 import jwt from 'jsonwebtoken';
-import { signinSchema, signupSchema } from '../validations/userValidation.js';
+import {
+  signinSchema,
+  signupSchema,
+  updateSchema,
+} from '../validations/userValidation.js';
 import { getRandomInRange } from '../utils/utils.js';
 
+//@desc     signup user
+//@route    POST /user/signup
+//@access   public
 export const signup = async (req, res) => {
   try {
+    const profile = req.file;
+    /* This code block is responsible for handling the signup functionality. */
     const { success, error } = signupSchema.safeParse(req.body);
     if (!success) {
       const message = error.formErrors.fieldErrors;
@@ -15,11 +24,15 @@ export const signup = async (req, res) => {
     if (existingUser) {
       return res.status(409).json({ message: 'Email already taken' });
     }
+
     const user = new User({
       ...req.body,
       lat: getRandomInRange(30.7, 30.77, 6), // Narrow latitude range for Chandigarh
       lang: getRandomInRange(76.75, 76.8, 6),
     });
+    if (profile) {
+      user.profile = profile.filename;
+    }
     const hashedPassword = await user.createHash(req.body.password);
     user.password = hashedPassword;
     (user.location = {
@@ -30,16 +43,30 @@ export const signup = async (req, res) => {
 
     const userId = user._id;
     const token = jwt.sign({ userId }, process.env.JWT_SECRET);
-    res.status(201).json({ message: 'User created Successfully', token });
+    res.status(201).json({
+      message: 'User created Successfully',
+      user: {
+        token,
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile,
+        zipCode: user.zipCode,
+        profile: user.profile,
+      },
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Enable to create User', error });
   }
 };
 
+//@desc     signin user
+//@route    POST /user/signin
+//@access   public
 export const signin = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const { success } = signinSchema.safeParse({ email, password });
     if (!success) {
       return res.status(400).json({ message: 'Invalid Inputs' });
@@ -51,7 +78,18 @@ export const signin = async (req, res) => {
     if (await user.validatePassword(password)) {
       const userId = user._id;
       const token = jwt.sign({ userId }, process.env.JWT_SECRET);
-      res.status(200).json({ token });
+
+      res.status(200).json({
+        message: 'Logged in Successfully',
+        user: {
+          token,
+          name: user.name,
+          email: user.email,
+          mobile: user.mobile,
+          zipCode: user.zipCode,
+          profile: user.profile,
+        },
+      });
     } else {
       return res.status(404).json({ message: 'Invalid Credentials' });
     }
@@ -60,28 +98,62 @@ export const signin = async (req, res) => {
   }
 };
 
+//@desc     update user details
+//@route    PUT /user/update
+//@access   protected
 export const updateUser = async (req, res) => {
   try {
-    // const { firstName, lastName, password } = req.body;
-    const { success, error } = signupSchema.safeParse(req.body);
+    const { name, email, password, zipCode, phone, mobile } = req.body;
+    const profile = req.file;
+
+    const { success, error } = updateSchema.safeParse(req.body);
+
     if (!success) {
       const message = error.formErrors.fieldErrors;
       return res.status(400).json({ message });
     }
-    const updateFields = req.body;
-    if (updateFields.password) {
+    const updateFields = {
+      name,
+      email,
+      zipCode,
+      phone,
+      mobile,
+    };
+    if (profile) {
+      updateFields.profile = profile.filename;
+    }
+    if (password.length > 6) {
       const user = await User.findOne({ _id: req.userId });
-      const hashedPassword = await user.createHash(updateFields.password);
+      const hashedPassword = await user.createHash(req.body.password);
       updateFields.password = hashedPassword;
     }
-    await User.findOneAndUpdate({ _id: req.userId }, updateFields);
-    res.status(200).json({ message: 'User updated Successfully' });
+    const user = await User.findOneAndUpdate(
+      { _id: req.userId },
+      updateFields,
+      { new: true }
+    );
+
+    const token = jwt.sign({ userId: req.userId }, process.env.JWT_SECRET);
+    res.status(201).json({
+      message: 'User Updated Successfully',
+      user: {
+        token,
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile,
+        zipCode: user.zipCode,
+        profile: user.profile,
+      },
+    });
   } catch (error) {
     console.log(error);
     return res.status(401).json({ message: 'Server Error' });
   }
 };
 
+//@desc     get nearby users
+//@route    POST /user/nearby-users
+//@access   protected
 export const getNearbyUsers = async (req, res) => {
   try {
     const currentUser = await User.findById(req.userId);
@@ -90,22 +162,20 @@ export const getNearbyUsers = async (req, res) => {
         $geoNear: {
           near: {
             type: 'Point',
-            coordinates: [currentUser.lang, currentUser.lat], // Use the current user's lang and lat
           },
-          distanceField: 'distance', // This field will show the distance to each user from the currentUser
-          maxDistance: 5000, // Maximum distance in meters
+          distanceField: 'distance',
           spherical: true,
-          query: { _id: { $ne: currentUser._id } }, // Exclude the current user
+          query: { _id: { $ne: currentUser._id } },
         },
       },
       {
         $project: {
-          _id: 0, // Exclude the _id field
-          name: 1, // Include the name field
-          profile: 1, // Include the profile field
-          email: 1, // Include the email field
-          phone: 1, // Include the phone field
-          location: 1, // Include the location field
+          _id: 0,
+          name: 1,
+          profile: 1,
+          email: 1,
+          phone: 1,
+          zipCode: 1,
         },
       },
       { $limit: 5 }, // Limit to 5 users
